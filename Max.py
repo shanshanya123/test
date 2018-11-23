617325766e1825b86ead08389548b9b323dd7bef
shanshanya123 <44665764+shanshanya123@users.noreply.github.com>
2018-11-23 16:01:49 +0800

    Find Max
    find max in an array

---
+++ Max.py
@@ -0,0 +1,10 @@
+def find_max(nums):
+    max=nums[0]
+    for x in nums:
+        if x>max:
+            max=x
+    print(max)
+def main():
+    find_max([55,41,32,89,77,51,20,33,66,88,2])
+if __name__ == '__main__':
+    main()
