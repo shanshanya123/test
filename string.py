2c6d9295fd504b8b44c66c4616d881b482c67d8c
shanshanya123 <44665764+shanshanya123@users.noreply.github.com>
2018-12-01 12:21:23 +0800

    Update string.py

--- string.py
+++ string.py
@@ -1,6 +1,7 @@
-str1="hello world"
+str1="hello world“
 str2="python"
 print("str1[0]:",str1[0])
 print("str2[0:5]:",str2[0:5])
-l=str1.__len__()
-print("更新字符串:",str1[:l]+str2)
+l=str1_len_()
+print("更新字符串:",str1[:l]+"haha")
+print("字符串拼接:",str1+str2)
