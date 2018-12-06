304d5cf31377d886525ec7f2a7bfcba488f9148f
shanshanya123 <44665764+shanshanya123@users.noreply.github.com>
2018-12-06 21:51:56 +0800

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,8 @@
 supplies=['pens','staplers','flame-throwers','binders']
 for i in range(len(supplies)):
     print('Index ' +str(i) +' in supplies is:'+supplies[i])
+
+if ('pens'in supplies):
+    print('ture')
+else:
+    print('false')
