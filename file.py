4edb655c160111c99e70187de2f99b33d66b2dff
shanshanya123 <44665764+shanshanya123@users.noreply.github.com>
2018-12-28 21:42:46 +0800

    Update file.py

--- file.py
+++ file.py
@@ -11,16 +11,16 @@ input("?")
 print "Opening the file..."
 target = open(filename, 'w+')
 
-print "Truncating the file. Goodbye!"
+print ("Truncating the file. Goodbye!")
 target.truncate()
 
-print "Now I'm going to ask you for three lines."
+print ("Now I'm going to ask you for three lines.")
 
-line1 = raw_input("line 1: ")
-line2 = raw_input("line 2: ")
-line3 = raw_input("line 3: ")
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
 
-print "I'm going to write these to the file."
+print ("I'm going to write these to the file.")
 
 target.write(line1)
 target.write("\n")
@@ -28,7 +28,5 @@ target.write(line2)
 target.write("\n")
 target.write(line3)
 target.write("\n")
-# 替换上面的写入
-# target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
 print "And finally, we close it."
 target.close()
