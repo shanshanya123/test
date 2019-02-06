70b9bea9973f34d84f51061d018e27e191e226dc
shanshanya123 <44665764+shanshanya123@users.noreply.github.com>
2019-02-06 19:14:17 +0800

    Update zhengze.py

--- zhengze.py
+++ zhengze.py
@@ -1,4 +1,4 @@
 import re
 phonenum=re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo =phonenum.search('My number is 415-555-6262.')
+mo =phonenum.search('My number is 415-666-6262.')
 print('Phone number found:'+mo.group())
